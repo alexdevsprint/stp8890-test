73e46f9b459c3fed750e9190a73ac83a3b4b136a
alexdevsprint <alexdevsprint@gmail.com>
2025-05-06 10:55:05 +0300

    commit

--- src/js/main-characters.js
+++ src/js/main-characters.js
@@ -9,28 +9,28 @@ const base = window.location.hostname === 'localhost' ? '' : '/stp8890-test';
 const data = [
   {
     name: 'MOO',
-    avatarMob: `${base}/img/main-characters/main-characters-moo-mob.png`,
-    avatarDesk: `${base}/img/main-characters/main-characters-moo-desk.png`,
+    avatarMob: '/img/main-characters/main-characters-moo-mob.png',
+    avatarDesk: '/img/main-characters/main-characters-moo-desk.png',
     description:
       'A clever guardian with control over elemental magic. He uses spells to slow and confuse enemies.',
   },
   {
     name: 'LULU',
-    avatarMob: `${base}/img/main-characters/main-characters-lulu-mob.png`,
-    avatarDesk: `${base}/img/main-characters/main-characters-lulu-desk.png`,
+    avatarMob: '/img/main-characters/main-characters-lulu-mob.png',
+    avatarDesk: '/img/main-characters/main-characters-lulu-desk.png',
     description:
       'Agile and fierce, Lulu’s tower upgrades pack a punch — she leads the charge with precision and power.',
   },
   {
     name: 'ELDER BRAMBLE',
-    avatarMob: `${base}/img/main-characters/main-characters-bramble-mob.png`,
-    avatarDesk: `${base}/img/main-characters/main-characters-bramble-desk.png`,
+    avatarMob: '/img/main-characters/main-characters-bramble-mob.png',
+    avatarDesk: '/img/main-characters/main-characters-bramble-desk.png',
     description: 'Mysterious druid who provides upgrades and guidance.',
   },
   {
     name: 'THE FOGLINGS',
-    avatarMob: `${base}/img/main-characters/main-characters-foglings-mob.png`,
-    avatarDesk: `${base}/img/main-characters/main-characters-foglings-desk.png`,
+    avatarMob: '/img/main-characters/main-characters-foglings-mob.png',
+    avatarDesk: '/img/main-characters/main-characters-foglings-desk.png',
     description:
       'Mischievous but harmless spirits that help collect offline rewards.',
   },
@@ -70,6 +70,7 @@ function renderItems(data) {
       ({ name, avatarMob, avatarDesk, description }) => {
         
         let classBackImg = '';
+        
 
     switch (name) {
       case 'MOO':
@@ -92,9 +93,9 @@ function renderItems(data) {
           <li class="swiper-slide main-characters-slider-item ${classBackImg}">
           <p class="main-characters-slider-item-title">${name}</p>  
           <img class="main-characters-slider-img"
-              src="${avatarDesk}"
-               srcset="${avatarMob} 320w,
-              ${avatarDesk} 1200w"
+              src="${base + avatarDesk}"
+               srcset="${base + avatarMob} 320w,
+              ${base + avatarDesk} 1200w"
                sizes="(max-width: 1199px) 320px, 1200px"
                alt="Avatar" 
             />             
